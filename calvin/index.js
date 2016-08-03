b15fcd9d4974863bd4ffe8a74cd28b74375ad3c1
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-08-03 00:04:26 +0000

    get jquery working verify bootstrap working

--- calvin/index.js
+++ calvin/index.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
     console.log("the document is ready");
+    $selected = $('.selected');
+    $selected.html("wrote this with jQuery");
+    console.log($selected);
 });
